3585567d20e16905d14c84905c40ef7702c7c009
Yunan Helmy <helmy@extrainteger.com>
2019-01-14 04:14:50 +0700

    implement monkey patch Alam YBS

--- lib/grape/pagination.rb
+++ lib/grape/pagination.rb
@@ -5,9 +5,9 @@ module Grape
         def paginate(collection)
           per_page = ApiPagination.config.per_page_param(params) || route_setting(:per_page)
 
-          options = {
-            :page     => ApiPagination.config.page_param(params),
-            :per_page => [per_page, route_setting(:max_per_page)].compact.min
+          options          = {
+              :page     => ApiPagination.config.page_param(params),
+              :per_page => [per_page, route_setting(:max_per_page)].compact.min
           }
           collection, pagy = ApiPagination.paginate(collection, options)
 
@@ -26,11 +26,32 @@ module Grape
           page_header     = ApiPagination.config.page_header
           include_total   = ApiPagination.config.include_total
 
-          header 'Link',          links.join(', ') unless links.empty?
-          header total_header,    ApiPagination.total_from(pagy || collection).to_s if include_total
+          header 'Link', links.join(', ') unless links.empty?
+          header total_header, ApiPagination.total_from(pagy || collection).to_s if include_total
           header per_page_header, options[:per_page].to_s
-          header page_header,     options[:page].to_s unless page_header.nil?
+          header page_header, options[:page].to_s unless page_header.nil?
 
+          if [:pagy, "pagy"].include?(ApiPagination.config.paginator)
+            Array.class_eval do
+              def pagy(pagy = nil)
+                @pagy = pagy
+              end
+
+              def total_pages
+                @pagy.pages if @pagy.present? && @pagy.class.name.eql?("Pagy")
+              end
+
+              def limit_value
+                @pagy.items if @pagy.present? && @pagy.class.name.eql?("Pagy")
+              end
+
+              def current_page
+                @pagy.page if @pagy.present? && @pagy.class.name.eql?("Pagy")
+              end
+            end
+            collection = Array.new(collection)
+            collection.pagy(pagy)
+          end
           return collection
         end
       end
@@ -41,14 +62,14 @@ module Grape
           route_setting :max_per_page, options[:max_per_page]
 
           enforce_max_per_page = options[:max_per_page] && options[:enforce_max_per_page]
-          per_page_values = enforce_max_per_page ? 0..options[:max_per_page] : nil
+          per_page_values      = enforce_max_per_page ? 0..options[:max_per_page] : nil
 
           params do
-            optional :page,     :type   => Integer, :default => 1,
-                                :desc   => 'Page of results to fetch.'
-            optional :per_page, :type   => Integer, :default => options[:per_page],
-                                :desc   => 'Number of results to return per page.',
-                                :values => per_page_values
+            optional :page, :type => Integer, :default => 1,
+                     :desc        => 'Page of results to fetch.'
+            optional :per_page, :type => Integer,
+                     :desc            => 'Number of results to return per page.',
+                     :values          => per_page_values
           end
         end
       end
